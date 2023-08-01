6e79dab94f083811afc366db394495b280df3e1c
dharmesh <onlinedk2k@gmail.com>
2023-08-01 13:55:42 +0530

    fasda

--- server.js
+++ server.js
@@ -4,18 +4,8 @@ var cors = require("cors");
 // const bodyParser = require("body-parser");
 require("./database");
 const port = 5000;
-var whitelist = ["https://memoapp-backend.vercel.app"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Something went wrong"));
-    }
-  },
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 // isme saare routes honge for project
 app.use("/api/auth", require("./routes/auth"));
